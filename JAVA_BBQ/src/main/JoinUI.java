0126477ff0d2f25e55abe2c4e35a568fa1ea4129
Seoyeon115 <binzang11@naver.com>
2021-04-09 16:26:49 +0900

    20210409

--- JAVA_BBQ/src/main/JoinUI.java
+++ JAVA_BBQ/src/main/JoinUI.java
@@ -9,14 +9,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+
+import BBQ_VO.MemberVO;
 
 public class JoinUI implements ActionListener {
 	// Field
@@ -24,9 +28,10 @@ public class JoinUI implements ActionListener {
 	JPanel title_panel, label_panel, tf_panel, btn_panel;
 	JButton btn_join, btn_reset, btn_id_chk;
 	String[] namelist = { "아이디", "비밀번호", "비밀번호확인", "이름", "핸드폰", "주소" };
+	ArrayList<Object> list = new ArrayList<Object>();
 	LoginUI log;
 //	StartUIEvent eventobj = new StartUIEvent(this);
-	
+
 	// Constructor
 	public JoinUI() {
 		init();
@@ -36,7 +41,7 @@ public class JoinUI implements ActionListener {
 		this.log = log;
 		init();
 	}
-	
+
 	// Method
 	public void init() {
 		f = new JFrame("BBQ");
@@ -77,6 +82,7 @@ public class JoinUI implements ActionListener {
 				t_panel.add(id);
 				t_panel.add(btn_id_chk);
 				tf_panel.add(t_panel);
+				list.add(id);
 
 			} else if (name.equals("핸드폰")) {
 				JTextField hp1 = new JTextField(6);
@@ -98,10 +104,12 @@ public class JoinUI implements ActionListener {
 				JPasswordField tf = new JPasswordField(20);
 				t_panel.add(tf);
 				tf_panel.add(t_panel);
+				list.add(tf);
 			} else {
 				JTextField tf = new JTextField(20);
 				t_panel.add(tf);
 				tf_panel.add(t_panel);
+				list.add(tf);
 			}
 		} // for
 
@@ -120,7 +128,7 @@ public class JoinUI implements ActionListener {
 
 			}
 		});
-		
+
 		btn_join.addActionListener(this);
 		btn_reset.addActionListener(this);
 		btn_id_chk.addActionListener(this);
@@ -136,14 +144,45 @@ public class JoinUI implements ActionListener {
 		Object obj = e.getSource();
 		String name = e.getActionCommand().trim();
 
+		
 		if (obj == btn_join) {
+			form_check();
 			// 가입정보 DB 등록
 		} else if (obj == btn_reset) {
 			
 		} else if (obj == btn_id_chk) {
+			
+		} else if (obj == btn_reset) {
+			
+			for(Object obj2 : list) {
+				JTextField tf = (JTextField)obj2;
+				tf.setText("");}
+
+		} else if (obj == btn_id_chk) {
 			// DB아이디 중복체크
+			System.out.println("아이디 중복체크");
 
 		}
+	}
+
+	/** 회원가입 폼 체크 **/
+
+	public boolean form_check() {
+		boolean result = false;
+
+		for (int i = 0; i < namelist.length - 2; i++) {
+			JTextField tf = (JTextField) list.get(i);
+
+			if (tf.getText().equals("")) {
+				JOptionPane.showMessageDialog(null, Commons.getMsg(namelist[i] + "를 입력해주세요"));
+				tf.requestFocus();
+				i = namelist.length - 2;
+			} else if (i == namelist.length - 3) {
+				result = true;
+			}
+		}
+
+		return result;
 	}
 
 }
