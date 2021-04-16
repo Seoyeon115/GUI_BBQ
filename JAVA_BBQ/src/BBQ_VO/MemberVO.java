b48ed881417ead0850f0a728776ab7fea24bf10d
jsggo2001 <jsggo2001@naver.com>
2021-04-16 18:54:57 +0900

    04/16

--- JAVA_BBQ/src/BBQ_VO/MemberVO.java
+++ JAVA_BBQ/src/BBQ_VO/MemberVO.java
@@ -1,4 +1,78 @@
 package BBQ_VO;
 
 public class MemberVO {
+	String id,pass,cpass,name,hp1,hp2,hp3,addr1,addr2;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getCpass() {
+		return cpass;
+	}
+
+	public void setCpass(String cpass) {
+		this.cpass = cpass;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHp1() {
+		return hp1;
+	}
+
+	public void setHp1(String hp1) {
+		this.hp1 = hp1;
+	}
+
+	public String getHp2() {
+		return hp2;
+	}
+
+	public void setHp2(String hp2) {
+		this.hp2 = hp2;
+	}
+
+	public String getHp3() {
+		return hp3;
+	}
+
+	public void setHp3(String hp3) {
+		this.hp3 = hp3;
+	}
+
+	public String getAddr1() {
+		return addr1;
+	}
+
+	public void setAddr1(String addr1) {
+		this.addr1 = addr1;
+	}
+
+	public String getAddr2() {
+		return addr2;
+	}
+
+	public void setAddr2(String addr2) {
+		this.addr2 = addr2;
+	}
+	
 }
